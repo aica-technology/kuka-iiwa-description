a3337b571387bc6b35e35cc1bbf23c82c944e6a9
Dominic Reber <dominic.reber@epfl.ch>
2021-11-17 11:15:55 +0100

    Remove Dockerfile and add run script with disclaimer instead

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is for internal development purposes, and does
+# not run for the general public due to the fact that the
+# image 'aica-technology/hardware-descriptions is not public.
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+aica-docker interactive aica-technology/hardware-descriptions:latest -u ros2 \
+  --volume="${SCRIPT_DIR}":/home/ros2/ros2_ws/src/iiwa/kuka_iiwa_description:rw
