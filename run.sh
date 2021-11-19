24b965bcf41f41efed4b0185406485e754fde322
Dominic Reber <dominic.reber@epfl.ch>
2021-11-19 13:01:06 +0100

    Fix path because of new hardware descriptions structure

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 aica-docker interactive aica-technology/hardware-descriptions:latest -u ros2 \
-  --volume="${SCRIPT_DIR}":/home/ros2/ros2_ws/src/iiwa/kuka_iiwa_description:rw
+  --volume="${SCRIPT_DIR}":/home/ros2/ros2_ws/src/kuka_iiwa_description:rw
